1cd5e2081f8b4adf9db633226cb563d0dccbce54
sergey-shiryaev <s.shiryaev96@mail.ru>
2022-03-13 20:44:45 +0300

    Second lab added

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -2,15 +2,10 @@ package ru.netology
 
 fun main() {
 
-    val commissionPercent = 0.0075
-    val commissionMin = 35
-    val transactionMoney = 10_000
-    val commission = if (transactionMoney * commissionPercent > commissionMin) {
-        transactionMoney * commissionPercent
-    } else commissionMin
-    val amount = transactionMoney - commission.toInt()
+    val likes = 21
+    val peopleEnding = if ((likes-1) % 10 == 0 && likes != 11 || likes == 1) {
+        "человеку"
+    } else "людям"
 
-    println("Your commission is ${commission.toInt()}")
-    println("Amount for transaction is $amount")
-
+    println("Понравилось $likes $peopleEnding")
 }
