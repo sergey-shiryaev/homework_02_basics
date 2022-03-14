b1359062de0e2ca6a208b311002274a8c1443c15
sergey-shiryaev <s.shiryaev96@mail.ru>
2022-03-14 18:20:34 +0300

    Third lab added

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -1,11 +1,21 @@
 package ru.netology
 
+import kotlin.math.roundToInt
+
 fun main() {
-
-    val likes = 21
-    val peopleEnding = if ((likes-1) % 10 == 0 && likes != 11 || likes == 1) {
-        "человеку"
-    } else "людям"
-
-    println("Понравилось $likes $peopleEnding")
+    val standardDiscount = 100
+    val lastPurchase = 10_010
+    val purchase = 100
+    val discount = 0.05
+    val regularCustomer = true
+    val discountForRegularCustomers = 0.01
+    val amount:Double = when (lastPurchase) {
+        in (0..1000) -> purchase.toDouble()
+        in (1001..10_000) -> (purchase - standardDiscount).toDouble()
+        else -> purchase * (1 - discount)
+    }
+    val amountWithDiscount = if (regularCustomer) amount * (1 - discountForRegularCustomers) else amount
+    val rub = amountWithDiscount.toInt()
+    val penny = ((amountWithDiscount - rub) * 100).roundToInt()
+    println("Стоимость Вашей покупки $rub рублей $penny копеек")
 }
